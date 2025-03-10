e9659a35adf548d2e5ba4222e9a62d979a5f4d07
Alisson990jd <116843255+Alisson990jd@users.noreply.github.com>
2025-03-10 17:44:57 -0300

    Update auto_gmail_creator.py

--- auto_gmail_creator.py
+++ auto_gmail_creator.py
@@ -7,7 +7,6 @@ print("""
             ██║   ██║ ██║╚██╔╝██║██╔══██║██║     
             ╚██████╔╝ ██║ ╚═╝ ██║██║  ██║███████╗
              ╚═════╝  ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝
-                                                  
        Automated Gmail Account Creator - By SHADOWHACKER
        Website: https://www.shadowhackr.com
 ===================================================
@@ -24,6 +23,8 @@ import time
 import requests
 from unidecode import unidecode
 import uuid
+import tempfile
+import os
 from fp.fp import FreeProxy  # Import FreeProxy
 
 # User Agents list for random selection
@@ -67,8 +68,6 @@ user_agents = [
     "Mozilla/5.0 (X11; CrOS x86_64 14526.69.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.82 Safari/537.36",
     "Mozilla/5.0 (X11; CrOS x86_64 14695.25.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.22 Safari/537.36",
     "Mozilla/5.0 (X11; CrOS x86_64 14526.89.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.91 Safari/537.36"
-       
-    # Add more User Agents
 ]
 
 # Arabic names written in English letters
@@ -87,7 +86,6 @@ arabic_first_names = [
     "Safa", "Nabilah", "Hala", "Faten", "Aisha", "Hassan", "Zainab", "Nouran", "Raneem", "Youssef",
 ]
 
-
 arabic_last_names = [
     "Mohamed", "Ahmed", "Hussein", "Sayed", "Ismail", "Abdallah", "Khalil", "Soliman", "Nour", "Kamel",
     "Samir", "Ibrahim", "Othman", "Fouad", "Zaki", "Gamal", "Farid", "Mansour", "Adel", "Salem",
@@ -101,7 +99,6 @@ arabic_last_names = [
     "Ramzi", "Khalifa", "Hamed", "Anis", "Hussein", "Mahdi", "Samir", "Wahab", "Bakkar", "Najib",
     "Abdulhadi", "Alhaj", "Shahrani", "Sulieman", "Majeed", "Nazari", "Saber", "Tawfiq", "Sabry", "Sharif",
 ]
-
 
 # Function to get a working proxy
 def get_working_proxy():
@@ -134,9 +131,7 @@ def fill_form(driver):
         device_uuid = uuid.uuid4()
         print(f"Using device UUID: {device_uuid}")
         your_username = generate_username()
-
         driver.get("https://accounts.google.com/signup/v2/createaccount?flowName=GlifWebSignIn&flowEntry=SignUp")
-
         # Fill in the name fields
         first_name = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "firstName")))
         last_name = driver.find_element(By.NAME, "lastName")
@@ -144,10 +139,8 @@ def fill_form(driver):
         first_name.send_keys(your_username.split('.')[0])
         last_name.clear()
         last_name.send_keys(your_username.split('.')[1])
-
         next_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "VfPpkd-LgbsSe")))
         next_button.click()
-
         # Fill birthday and gender
         wait = WebDriverWait(driver, 20)
         day = wait.until(EC.visibility_of_element_located((By.NAME, "day")))
@@ -164,20 +157,17 @@ def fill_form(driver):
         gender_dropdown.select_by_value(your_gender)
         next_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "VfPpkd-LgbsSe")))
         next_button.click()
-
         # Create custom email
         time.sleep(2)
         if driver.find_elements(By.ID, "selectionc4"):
             create_own_option = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "selectionc4")))
             create_own_option.click()
-
         create_own_email = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "Username")))
         username_field = driver.find_element(By.NAME, "Username")
         username_field.clear()
         username_field.send_keys(your_username)
         next_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "VfPpkd-LgbsSe")))
         next_button.click()
-
         # Enter and confirm password
         password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "Passwd")))
         password_field.clear()
@@ -188,21 +178,17 @@ def fill_form(driver):
         password_confirmation_field.send_keys(your_password)
         next_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "VfPpkd-LgbsSe")))
         next_button.click()
-
         # Skip phone number and recovery email
         try:
             skip_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'Skip')]")))
             skip_button.click()
         except Exception as skip_error:
             print("No phone number verification step.")
-
         # Agree to terms
         agree_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button span.VfPpkd-vQzf8d")))
         agree_button.click()
-
         print(f"Your Gmail successfully created:\n{{\ngmail: {your_username}@gmail.com\npassword: {your_password}\n}}")
         save_email_to_file(f"{your_username}@gmail.com", your_password)
-
     except Exception as e:
         print("Failed to create your Gmail, Sorry")
         print(e)
@@ -212,7 +198,11 @@ def create_multiple_accounts(number_of_accounts):
     for i in range(number_of_accounts):
         chrome_options = ChromeOptions()
         chrome_options.add_argument("--disable-infobars")
-        chrome_options.add_argument("--user-data-dir=./cookies")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--headless")  # Run in headless mode
+        temp_dir = tempfile.mkdtemp()  # Create a unique temporary directory
+        chrome_options.add_argument(f"--user-data-dir={temp_dir}")
         user_agent = random.choice(user_agents)
         chrome_options.add_argument(f'user-agent={user_agent}')
         proxy = get_working_proxy()
